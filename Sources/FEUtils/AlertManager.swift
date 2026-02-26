71c902f66c5c116e5978b3d3209bd5acffd56875
mchm <m@mchm.pl>
2026-02-26 17:28:15 +0100

    Fixing

--- Sources/FEUtils/AlertManager.swift
+++ Sources/FEUtils/AlertManager.swift
@@ -9,41 +9,5 @@ import Combine
 
 @available(macOS 26, *)
 public class AlertManager: ObservableObject {
- @Published public var isPresented: Bool
- @Published public var title: String
- @Published public var message: String
- 
- public init(isPresented: Bool = false, title: String = "", message: String = "") {
-  self.isPresented = isPresented
-  self.title = title
-  self.message = message
- }
- 
- public func issueAlert(type: AlertType, entityName: String) {
-  title = "Issue occured when "
-  message = "Issue occured when trying to "
-  
-  if type == .save {
-   title += "saving data"
-   message += "save \(entityName)"
-  }
-  
-  if type == .fetch {
-   title += "downloading data"
-   message += "download \(entityName)"
-  }
-  
-  isPresented = true
- }
- 
- public func dismissAlert() {
-  isPresented = false
-  title = ""
-  message = ""
- }
-}
-
-public enum AlertType {
- case save
- case fetch
+ @Published public var isPresented = false
 }
